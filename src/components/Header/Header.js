595b23cb48e6976e449034c82cbd00c73400f8cc
Kayode Uthman <88583385+Utty007@users.noreply.github.com>
2023-06-12 02:06:22 -0700

    Added code for the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import ManageLogo from "../../../images/logo.svg";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="logo">
+        <img src={ManageLogo} />
+      </div>
+      <div className="links">
+        <span>Pricing</span>
+        <span>Products</span>
+        <span>About us</span>
+        <span>Careers</span>
+        <span>Community</span>
+      </div>
+      <div className="btn">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
